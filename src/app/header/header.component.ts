0e08ea9d9123e22f0ab4174dbc8574a3964db8e1
cpowers <cpowers@deloitte.com>
2016-06-20 14:14:31 -0400

    added the grey and green theme

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,12 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-header',
   templateUrl: 'header.component.html',
-  styleUrls: ['header.component.css', '../styles/template.css']
+  styleUrls: ['../styles/template.css', 'header.component.css'],
+  styles: [`
+  	.header {
+  		color: #fff;
+  	}
+  `]
 })
 export class HeaderComponent implements OnInit {
 
