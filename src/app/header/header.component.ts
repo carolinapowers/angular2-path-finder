dc67631cf97e3a6fa022795b0fece6f3c3ee09ae
cpowers <cpowers@deloitte.com>
2016-06-20 14:27:09 -0400

    fixed the width of nav bar

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,10 +4,11 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-header',
   templateUrl: 'header.component.html',
-  styleUrls: ['../styles/template.css', 'header.component.css'],
+  styleUrls: ['header.component.css'],
   styles: [`
   	.header {
   		color: #fff;
+  		background-color: blue;
   	}
   `]
 })
