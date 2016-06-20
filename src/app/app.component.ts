5436770ef4263fcd103e67ff25a19093dd60f1d9
cpowers <cpowers@deloitte.com>
2016-06-20 13:18:43 -0400

    added header without styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [FooterComponent]
+  directives: [FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'app works!';
 
-  constructor() {
+  constructor () {
+
   }
 }
+
+
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: 'app-header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css']
 })
-export class HeaderComponent  {
+export class HeaderComponent implements OnInit {
 
   constructor() {}
 
+	 ngOnInit() {
+  }
   
 
 }
