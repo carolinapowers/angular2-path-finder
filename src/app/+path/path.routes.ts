c304e9a66d254343fc3c82356ecec9c22c2dd77f
cpowers <cpowers@deloitte.com>
2016-06-21 09:50:06 -0400

    added home route to path route

--- src/app/+path/path.routes.ts
+++ src/app/+path/path.routes.ts
@@ -2,5 +2,6 @@ import { RouterConfig } from '@angular/router';
 import { PathComponent } from './';
 
 export const PATH_ROUTES: RouterConfig = [
-  { path: 'path', component: PathComponent }
+  { path: 'path', component: PathComponent },
+  { path: '', component: PathComponent}
 ];
